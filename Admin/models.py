46b178bcfb3e0e6ec8ba75da370c16c1a8df4c99
hxl <1357709111@qq.com>
2019-04-08 16:50:10 +0800

    登录

--- Admin/models.py
+++ Admin/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.hashers import make_password, check_password
 from django.db import models
 
 # Create your models here.
+from werkzeug.security import check_password_hash
+
+
+class AdminUser(models.Model):
+
+    a_username = models.CharField(max_length=15,unique=True)
+    a_password = models.CharField(max_length=256)
+
+    is_delete = models.BooleanField(default=False)
+    is_super = models.BooleanField(default=False)
+
+    def set_password(self,password):
+        self.a_password = make_password(password)
+
+    def check_admin_password(self,password):
+        return check_password(password,self.a_password)
